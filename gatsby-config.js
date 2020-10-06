34bdda9313fa206119df2342cc028a5c969c2d23
Randy Oest <oest@amazingrando.com>
2020-10-05 20:28:03 -0400

    Reconfigure for Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
   siteMetadata: {
     title: `For The Queen SRD`,
@@ -60,7 +62,18 @@ module.exports = {
       options: {
         spreadsheetId: "1bkubC3K04PKg3OH0dQTf8ZRcMcZpmAQGwwiHxFgmgug",
         worksheetTitle: "Form-Responses-1",
-        credentials: require("./src/env/For-The-Queen-3f3deedb6442.json"),
+        credentials: {
+          "type": "service_account",
+          "project_id": process.env.project_id,
+          "private_key_id": process.env.private_key_id,
+          "private_key": process.env.private_key,
+          "client_email": process.env.client_email,
+          "client_id": process.env.client_id,
+          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+          "token_uri": "https://oauth2.googleapis.com/token",
+          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+          "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/forthequeen2%40for-the-queen-239501.iam.gserviceaccount.com"
+        },
       },
     },
   ],
